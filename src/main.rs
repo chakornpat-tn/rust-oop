b445ed428c457b7e1f87cbec37d8d4cb2fda5947
chakornpat <chakornpat0206@outlook.com>
2025-03-31 21:26:26 +0700

    Refactor character classes to implement notifier system for health updates

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,38 @@
-trait Weapon {
-    fn attack(&self);
-}
-
-struct Sword;
-impl Weapon for Sword {
-    fn attack(&self) {
-        println!("Sword attack!");
-    }
-}
-
-struct Staff;
-impl Weapon for Staff {
-    fn attack(&self) {
-        println!("Staff attack!");
-    }
-}
-
-struct SwordMan {
-    health: u8,
-    strength: u8,
-    intelligence: u8,
-    weapon: Box<dyn Weapon>,
-}
-
-impl SwordMan {
-    fn new() -> Self {
-        Self {
-            health: 100,
-            strength: 20,
-            intelligence: 10,
-            weapon: Box::new(Sword),
-        }
-    }
-
-    fn health_increase(&mut self, value: u8) {
-        if self.health + value > 100 {
-            self.health = 100;
-            return;
-        }
-        self.health += value;
-    }
-
-    fn health_decrease(&mut self, value: u8) {
-        self.health = self.health.saturating_sub(value);
-    }
-}
-
-struct Mage {
-    health: u8,
-    strength: u8,
-    intelligence: u8,
-    weapon: Box<dyn Weapon>,
-}
-impl Mage {
-    fn new() -> Self {
-        Self {
-            health: 80,
-            strength: 10,
-            intelligence: 20,
-            weapon: Box::new(Staff),
-        }
-    }
-
-    fn health_increase(&mut self, value: u8) {
-        if self.health + value > 80 {
-            self.health = 80;
-            return;
-        }
-        self.health += value;
-    }
-
-    fn health_decrease(&mut self, value: u8) {
-        self.health = self.health.saturating_sub(value);
-    }
-}
-
-struct Healer {
-    health: u8,
-    strength: u8,
-    intelligence: u8,
-    weapon: Box<dyn Weapon>,
-}
-
-impl Healer {
-    fn new() -> Self {
-        Self {
-            health: 70,
-            strength: 5,
-            intelligence: 14,
-            weapon: Box::new(Staff),
-        }
-    }
-    fn health_increase(&mut self, value: u8) {
-        if self.health + value > 70 {
-            self.health = 70;
-            return;
-        }
-        self.health += value;
-    }
-    fn health_decrease(&mut self, value: u8) {
-       self.health = self.health.saturating_sub(value);
-    }
+trait Notifier {
+    fn notify(&self, message: &str);
     
+} 
+
+struct DMSNotifier;
+
+impl Notifier for DMSNotifier {
+    fn notify(&self, message: &str) {
+        println!("DMS Notification: {}", message);
+    }
 }
 
-fn  use_attack(weapon:Box<dyn Weapon>) {
-    weapon.attack();
+struct EmailNotifier;
+
+impl Notifier for EmailNotifier {
+    fn notify(&self, message: &str) {
+        println!("Email Notification: {}", message);
+    }
+}
+
+struct NotifierManager;
+impl NotifierManager {
+    fn send_notifier<T: Notifier>(&self, notifier: T) {
+        notifier.notify("completed");
+    }
 }
 
 fn main() {
-    let mut player1 = SwordMan::new();
-    let mut player2 = Mage::new();
-    let mut player3 = Healer::new();
+    let notify_manager = NotifierManager;
 
-    player1.health_decrease(10);
-    player2.health_decrease(10);
-    player3.health_decrease(10);
+    let dms_notifier = DMSNotifier;
+    let email_notifier = EmailNotifier;
 
-    println!("Player 1 health: {}", player1.health);
-    println!("Player 2 health: {}", player2.health);
-    println!("Player 3 health: {}", player3.health);
-
-    player1.health_increase(10);
-    player2.health_increase(5);
-    player3.health_increase(20);
-
-    println!("Player 1 health: {}", player1.health);
-    println!("Player 2 health: {}", player2.health);
-    println!("Player 3 health: {}", player3.health);
-
-    use_attack(player1.weapon);
-    use_attack(player2.weapon);
-    use_attack(player3.weapon);
-
+    notify_manager.send_notifier(dms_notifier);
+    notify_manager.send_notifier(email_notifier);
 
 }
